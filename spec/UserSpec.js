8e5d2bde2f604a93058f9065406c52f7706a20d6
Xa1n <xhs893@gmail.com>
2019-05-07 15:27:39 +0100

    Writes tests for User class, creates User class with name, email and password as instances
    Co-authored-by: Calum Darroch <calum_darroch@hotmail.com>

--- spec/UserSpec.js
+++ spec/UserSpec.js
@@ -1,3 +1,27 @@
+'use strict';
+
 describe("User", function() {
-  
+
+        var user;
+
+        beforeEach(function(){
+            user = new User('World', 'world@gmail.com', 'hello20');
+        });
+
+    describe('User initialises their account', function(){
+
+        it('with a username', function(){
+            expect(user.name).toEqual('World');
+        });
+
+        it('with an email', function(){
+            expect(user.email).toEqual('world@gmail.com');
+        });
+
+        it('with a passsword', function(){
+            expect(user._password).toEqual('hello20');
+        });
+
+    });
+
 });
---
+++ src/User.js
@@ -0,0 +1,6 @@
+function User(name, email, password) {       
+    this.name = name;       // User.name is the username that'll eventually be displayed
+    this.email = email;
+    this._password = password;
+};
+
