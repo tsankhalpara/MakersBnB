41dbbee3452d2f5bb101c9441b87a92103ef5715
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-09 16:31:24 +0100

    delete duplicate

--- db/migrations/03_insert_row_to_users.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "public"."users" ("email", "password", "username", "id")
-VALUES ('tsankhalpara@mac.com', 'makers', 'tara.s', '1');
-
-INSERT INTO "public"."users" ("password", "email", "id", "username")
-VALUES ('makers', 'calum_darroch@hotmail.com', '2', 'calum.d');
