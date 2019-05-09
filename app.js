57965ea16e5d07fe49b72bbb2021fb3b198ab010
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-09 15:58:33 +0100

    combine database connection with app.js

--- app.js
+++ app.js
@@ -6,7 +6,41 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+// database connection
+const app = express();
+var pg = require('pg');
+var format = require('pg-format');
+var PGUSER = 'Tara';
+var PGDATABASE = 'makersbnb';
+
+var config = {
+  user: PGUSER, // name of the user account
+  database: PGDATABASE, // name of the database
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+}
+
+var pool = new pg.Pool(config)
+var myClient
+
+// database test
+
+var username = "calum.d";
+
+pool.connect(function (err, client, done) {
+  if (err) console.log(err)
+  app.listen(3000, function () {
+    console.log('Connected successfully')
+  })
+  myClient = client
+  var nameQuery = format('SELECT * from users WHERE username = %L', username)
+  myClient.query(nameQuery, function (err, result) {
+    if (err) {
+      console.log(err)
+    }
+    console.log(result.rows[0])
+  })
+})
 
 // configure app
 
@@ -31,11 +65,6 @@ app.get('/', function (req, res) {
     title: 'Test',
     items: toDoItems
   });
-});
-
-
-app.listen(3000, function () {
-  console.log('ready on port 3000');
 });
 
 module.exports = app;
