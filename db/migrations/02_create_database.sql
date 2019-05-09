8183158c802414834884f4e01ad1584feb3c9175
Carly <carly.jenkinson65@googlemail.com>
2019-05-09 15:25:59 +0100

    added database table info

---
+++ db/migrations/02_create_database.sql
@@ -0,0 +1,2 @@
+CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(20),
+email VARCHAR(60), password VARCHAR(20));
---
+++ db/migrations/03_create_database.sql
@@ -0,0 +1,5 @@
+INSERT INTO "public"."users" ("email", "password", "username", "id")
+VALUES ('tsankhalpara@mac.com', 'makers', 'tara.s', '1');
+
+INSERT INTO "public"."users" ("password", "email", "id", "username")
+VALUES ('makers', 'calum_darroch@hotmail.com', '2', 'calum.d');
