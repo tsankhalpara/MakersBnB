f49bcd7e714e01f7a2661330a0a1210f51f4d969
Tara Sankhalpara <tsankhalpara@mac.com>
2019-05-10 10:47:50 +0100

    update css

--- app.js
+++ app.js
@@ -16,13 +16,12 @@ var PGDATABASE = 'makersbnb';
 var config = {
   user: PGUSER, // name of the user account
   database: PGDATABASE, // name of the database
-  max: 10, // max number of clients in the pool
+  max: 100, // max number of clients in the pool
   idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
 }
 
 var pool = new pg.Pool(config)
 var myClient
-
 // database test
 
 var username = "calum.d";
@@ -33,7 +32,7 @@ pool.connect(function (err, client, done) {
     console.log('Connected successfully')
   })
   myClient = client
-  var nameQuery = format('SELECT * from users WHERE username = %L', username)
+  var nameQuery = format('SELECT * from users WHERE username = %L', username);
   myClient.query(nameQuery, function (err, result) {
     if (err) {
       console.log(err)
